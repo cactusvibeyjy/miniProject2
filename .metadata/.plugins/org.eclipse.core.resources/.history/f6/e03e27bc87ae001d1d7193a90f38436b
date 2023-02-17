86cf87ade2eb5a8c3705ed22f5551309d204195c
YJY <xhrtlr@gmail.com>
2023-02-17 23:35:29 +0900

    미션2_빈로우추가 팝업수정etc

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f6/e03e27bc87ae001d1d7193a90f38436b
@@ -0,0 +1,15 @@
+package com.example.service;
+
+import com.example.dto.MemberDto;
+
+import java.util.List;
+
+public interface MemberService {
+
+    public List<MemberDto> memberList(MemberDto param) throws Exception;
+    public MemberDto memberDetail(String memberId) throws Exception;
+    public int memberInsert(MemberDto param) throws Exception;
+    public int memberUpdate(MemberDto param) throws Exception;
+    public int memberDelete(MemberDto param) throws Exception;
+
+}
---
+++ test2/src/main/java/com/example/service/impl/MemberServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.service.impl;
+
+import com.example.dao2.MemberDao;
+import com.example.dto.MemberDto;
+import org.springframework.stereotype.Service;
+import com.example.service.MemberService;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service("memberService")
+public class MemberServiceImpl implements MemberService{
+
+    @Resource(name = "memberDao") MemberDao memberDao;
+
+    @Override
+    public List<MemberDto> memberList(MemberDto param) throws Exception {
+        List<MemberDto> lists = memberDao.memberList(param);
+        return memberDao.memberList(param);
+    }
+
+    @Override
+    public MemberDto memberDetail(String memberId) throws Exception {
+        return memberDao.memberDetail(memberId);
+    }
+    
+    @Override
+	public int memberDupCheck(String memberId) throws Exception {
+		return memberDao.memberDupCheck(memberId);
+	}
+
+    @Override
+    public int memberInsert(MemberDto param) throws Exception {
+        return memberDao.memberInsert(param);
+    }
+
+    @Override
+    public int memberUpdate(MemberDto param) throws Exception {
+        return memberDao.memberUpdate(param);
+    }
+
+    @Override
+    public int memberDelete(MemberDto param) throws Exception {
+        return memberDao.memberDelete(param);
+    }
+
+}
